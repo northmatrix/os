716c33eb09c0f44cf63433827403501ba526f8b9
Northmatrix <contact@northmatrix.co.uk>
2025-04-13 17:05:51 +0000

    backspace sss and libgcc setup save before higher halfing kernel

--- string.h
+++ string.h
@@ -2,5 +2,9 @@
 #define INCLUDE_STRING_H
 
 unsigned int strlen(const char* data);
-
+void *memcpy(void *dest, const void *src, unsigned long n);
+void *memmove(void *dest, const void *src, unsigned long n);
+void *memset(void *s, int c, unsigned long n);
+int memcmp(const void *s1, const void *s2, unsigned long n);
+void abort(void);
 #endif
